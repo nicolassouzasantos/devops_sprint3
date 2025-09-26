fb0c3f7b7006991cbdc6e440febe1840959293de
nicolassouzasantos <nicklite1@hotmail.com>
2025-09-26 04:14:24 -0300

    arrumando erro

--- src/main/java/br/com/solutionsnote/note/Hash.java
+++
@@ -1,9 +0,0 @@
-package br.com.solutionsnote.note;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class Hash {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
-        System.out.println(new BCryptPasswordEncoder().encode("user123"));
-    }
-}
