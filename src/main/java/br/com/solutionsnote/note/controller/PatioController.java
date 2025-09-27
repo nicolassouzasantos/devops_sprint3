c0427e498dbc0bf7194ba903b7b62f3f615855d6
Nicolas Souza dos Santos <135378650+nicolassouzasantos@users.noreply.github.com>
2025-09-27 17:11:50 -0300

    Update PatioController.java

--- src/main/java/br/com/solutionsnote/note/controller/PatioController.java
+++ src/main/java/br/com/solutionsnote/note/controller/PatioController.java
@@ -4,6 +4,7 @@ import br.com.solutionsnote.note.model.Patio;
 import br.com.solutionsnote.note.repository.PatioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,28 @@ public class PatioController {
     @GetMapping
     public List<Patio> listar() {
         return repository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patio> atualizar(@PathVariable Long id,
+                                           @RequestBody @Valid Patio patioAtualizado) {
+        return repository.findById(id)
+                .map(patio -> {
+                    patio.setNome(patioAtualizado.getNome());
+                    patio.setEndereco(patioAtualizado.getEndereco());
+                    Patio salvo = repository.save(patio);
+                    return ResponseEntity.ok(salvo);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build()); // <- use Supplier aqui
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(patio -> {
+                    repository.delete(patio); // (ou deleteById(id))
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
