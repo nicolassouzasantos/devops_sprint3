59f2778fa099c6b380774811c8464385a96932f2
Nicolas Souza dos Santos <135378650+nicolassouzasantos@users.noreply.github.com>
2025-09-27 22:40:26 -0300

    Update OperadorController.java

--- src/main/java/br/com/solutionsnote/note/controller/OperadorController.java
+++ src/main/java/br/com/solutionsnote/note/controller/OperadorController.java
@@ -4,6 +4,7 @@ import br.com.solutionsnote.note.model.Operador;
 import br.com.solutionsnote.note.repository.OperadorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,31 @@ public class OperadorController {
     @GetMapping
     public List<Operador> listar() {
         return repository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Operador> atualizar(@PathVariable Long id, @RequestBody @Valid Operador operadorAtualizado) {
+        return repository.findById(id)
+                .map(operadorExistente -> {
+                    operadorExistente.setNome(operadorAtualizado.getNome());
+                    operadorExistente.setLogin(operadorAtualizado.getLogin());
+
+                    if (operadorAtualizado.getSenha() != null && !operadorAtualizado.getSenha().isBlank()) {
+                        operadorExistente.setSenha(operadorAtualizado.getSenha());
+                    }
+                    Operador salvo = repository.save(operadorExistente);
+                    return ResponseEntity.ok(salvo);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(operador -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.noContent().build(); // HTTP 204
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
